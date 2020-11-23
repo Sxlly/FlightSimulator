ef932b5f3af157c754f426b277442b6a5f84f36b
Sxlly <shaesullivan.buisness1@gmail.com>
2020-11-23 21:03:51 +0800

    more kivy testing

--- GUI.py
+++ GUI.py
@@ -5,12 +5,29 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 
 
+class MyGrid(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(MyGrid, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="Number Of Aircraft: "))
+        self.name = TextInput(multiline = False)
+        self.add_widget(self.name)
+
+        self.add_widget(Label(text="Aircraft Name: "))
+        self.aircraftOne = TextInput(multiline = False)
+        self.add_widget(self.aircraftOne)
+
+        self.add_widget(Label(text="Aircraft Name 2: "))
+        self.aircraftTwo = TextInput(multiline = False)
+        self.add_widget(self.aircraftTwo)
+
+
 class MyApp(App):
 
     def build(self):
 
-        return Label(text = "Flight Simulator")
-
+        return MyGrid()
 
 if __name__ == "__main__":
     MyApp().run()
