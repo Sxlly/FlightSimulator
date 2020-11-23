12509e4d564e4c21b2d86476131334eb51375502
Sxlly <shaesullivan.buisness1@gmail.com>
2020-11-23 20:55:21 +0800

    kivy test initial

---
+++ GUI.py
@@ -0,0 +1,16 @@
+import kivy
+from kivy.app import App
+from kivy.uix.label import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
+
+
+class MyApp(App):
+
+    def build(self):
+
+        return Label(text = "Flight Simulator")
+
+
+if __name__ == "__main__":
+    MyApp().run()
