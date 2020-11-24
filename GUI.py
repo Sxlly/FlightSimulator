d1556ef72dd4cd8490a410d26fabacf9b72e5453
Sxlly <shaesullivan.buisness1@gmail.com>
2020-11-24 11:41:06 +0800

    kivy object added

--- GUI.py
+++ GUI.py
@@ -4,11 +4,14 @@ from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
-from kivy.uix.widget import Widget 
+from kivy.uix.widget import Widget
+from kivy.properties import ObjectProperty
 
 
 class MyGrid(Widget):
+    n_aircrafts = ObjectProperty(None)
     pass
+
 
 
 class MyApp(App):
