3b08c167330054ab4324a299d9797c3469090252
Sxlly <shaesullivan.buisness1@gmail.com>
2020-11-24 12:14:17 +0800

    float layout added

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@ import kivy
 from kivy.app import App
 from kivy.uix.label import Label
 from kivy.uix.gridlayout import GridLayout
+from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
 from kivy.uix.widget import Widget
@@ -17,12 +18,12 @@ class MyGrid(Widget):
         self.n_aircrafts.text = ""
 
 
-
 class MyApp(App):
 
     def build(self):
 
         return MyGrid()
+        return FloatLayout()
 
 if __name__ == "__main__":
     MyApp().run()
